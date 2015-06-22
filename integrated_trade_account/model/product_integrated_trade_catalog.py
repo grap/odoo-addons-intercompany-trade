76a04b050d7c1f3f76a2a88075467f24924401eb
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-06-22 22:31:33 +0200

    [REF]

--- integrated_trade_account/model/product_integrated_trade_catalog.py
+++ integrated_trade_account/model/product_integrated_trade_catalog.py
@@ -20,8 +20,6 @@
 #
 ##############################################################################
 
-#from openerp import SUPERUSER_ID
-#from openerp import tools
 from openerp.osv import fields
 from openerp.osv.orm import Model
 from openerp.addons import decimal_precision as dp
@@ -30,52 +28,10 @@ from openerp.addons import decimal_precision as dp
 class ProductIntegratedTradeCatalog(Model):
     _inherit = 'product.integrated.trade.catalog'
 
-#    # Custom Section
-#    def _get_supplier_product_id_from_id(self, str_id):
-#        return int(str_id[:-4])
-
-#    def _get_integrated_trade_id_from_id(self, str_id):
-#        return int(str_id[-4:])
-
-#    # Button Section
-#    def link_product_wizard(self, cr, uid, ids, context=None):
-#        return {
-#            'view_type': 'form',
-#            'view_mode': 'form',
-#            'res_model': 'integrated.trade.wizard.link.product',
-#            'type': 'ir.actions.act_window',
-#            'target': 'new',
-#            'context': context,
-#        }
-
-#    def unlink_product(self, cr, uid, ids, context=None):
-#        psi_obj = self.pool['product.supplierinfo']
-#        for id in ids:
-#            supplier_product_id = self._get_supplier_product_id_from_id(id)
-#            psi_ids = psi_obj.search(cr, uid, [
-#                ('supplier_product_id', '=', supplier_product_id)],
-#                context=context)
-#            psi_obj.unlink(cr, uid, psi_ids, context=context)
-#        return True
-
-#    # Fields Function Section
-#    def _get_supplier_price(self, cr, uid, ids, name, arg, context=None):
-#        ppl_obj = self.pool['product.pricelist']
-#        res = {}
-#        for pitc in self.browse(cr, SUPERUSER_ID, ids, context=context):
-#            res[pitc.id] = ppl_obj._compute_integrated_prices(
-#                cr, SUPERUSER_ID, pitc.supplier_product_id,
-#                pitc.supplier_partner_id, pitc.pricelist_id, context=context)
-#        return res
-
     # Fields Function Section
     def _get_supplier_price(self, cr, uid, ids, name, arg, context=None):
-        
-        res = super(ProductIntegratedTradeCatalog, self)._get_supplier_price(
+        return super(ProductIntegratedTradeCatalog, self)._get_supplier_price(
             cr, uid, ids, name, arg, context=context)
-        print "OVERLOAD : RES"
-        print res
-        return res
 
     # Column Section
     _columns = {
