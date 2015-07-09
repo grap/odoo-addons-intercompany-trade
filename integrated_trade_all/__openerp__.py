41490cf1e1f47bf3c0f7f626d28b17a463c354c0
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-07-09 10:31:10 +0200

    [ADD] New module integrated_trade_all;

---
+++ integrated_trade_all/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Integrated Trade - ALL module for OpenERP
+#    Copyright (C) 2015-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Integrated Trade - ALL',
+    'version': '1.0',
+    'category': 'Integrated Trade',
+    'description': """
+Module for Integrated Trade
+===========================
+
+Copyright, Authors and Licence:
+-------------------------------
+    * Copyright: 2015, GRAP: Groupement Régional Alimentaire de Proximité;
+    * Author:
+        * Sylvain LE GAL (https://twitter.com/legalsylvain);
+    * Licence: AGPL-3 (http://www.gnu.org/licenses/);""",
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'integrated_trade_base',
+        'integrated_trade_product',
+        'integrated_trade_stock',
+        'integrated_trade_purchase_sale',
+        'integrated_trade_account',
+    ],
+}
