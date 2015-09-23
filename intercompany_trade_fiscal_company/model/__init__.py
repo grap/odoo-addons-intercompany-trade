21527d95c57e7b011df1979ca50b6dafa8c01b44
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-09-23 23:41:32 +0200

    [ADD] manage transcoded journal;

--- intercompany_trade_fiscal_company/model/__init__.py
+++ intercompany_trade_fiscal_company/model/__init__.py
@@ -20,8 +20,9 @@
 #
 ##############################################################################
 
-from . import fiscal_company_transcoding_account
+from . import account_invoice
 from . import account_invoice_line
+from . import fiscal_company_transcoding_account
 from . import intercompany_trade_config
 from . import stock_picking
 from . import sale_order_line
