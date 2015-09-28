1b900071f0f4a13c4b714680737977a163ecf7bd
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-09-28 20:25:02 +0200

    [ADD] module glue module with purchase_package_qty;

---
+++ intercompany_trade_purchase_package_qty/models/__init__.py
@@ -0,0 +1,2 @@
+# -*- encoding: utf-8 -*-
+from . import intercompany_trade_config
---
+++ intercompany_trade_purchase_package_qty/models/intercompany_trade_config.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Intercompany Trade - Purchase Package Quantity module for Odoo
+#    Copyright (C) 2015-Today GRAP (http://www.grap.coop)
+#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv.orm import Model
+
+
+class intercompany_trade_config(Model):
+    _inherit = 'intercompany.trade.config'
+
+    # Custom Section
+    def _prepare_product_supplierinfo(
+            self, cr, uid, id, supplier_product_id, customer_product_id,
+            context=None):
+        res = super(
+            intercompany_trade_config, self)._prepare_product_supplierinfo(
+                cr, uid, id, supplier_product_id, customer_product_id,
+                context=context)
+        res['indicative_package'] = True
+
+        return res
