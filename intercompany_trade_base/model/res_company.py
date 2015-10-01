0c90643de3dca4ec7aaa19f8da2144b93582911c
Sylvain LE GAL <sylvain.legal@grap.coop>
2015-10-01 23:49:24 +0200

    [FIX] prepare function to create partner from companies;

--- intercompany_trade_base/model/res_company.py
+++ intercompany_trade_base/model/res_company.py
@@ -34,23 +34,33 @@ class res_company(Model):
         rit_obj = self.pool['intercompany.trade.config']
         rp_obj = self.pool['res.partner']
         for rc in self.browse(cr, uid, ids, context=context):
+            ctx = context.copy()
+            ctx['ignore_intercompany_trade_check'] = True
             # Get customer partner created for this company
             rit_1 = rit_obj.browse(cr, uid, rit_obj.search(
                 cr, uid, [('supplier_company_id', '=', rc.id)],
                 context=context), context=context)
-            supplier_partner_ids = [
-                x.supplier_partner_id.id for x in rit_1]
+
+            for rit in rit_1:
+                # Update all the partner with updated information
+                data = rit_obj._prepare_partner_from_company(
+                    cr, uid, rc.id, rit.customer_company_id.id,
+                    context=context)
+                rp_obj.write(
+                    cr, rit.customer_user_id.id, [rit.supplier_partner_id.id],
+                    data, context=ctx)
+
             # Get supplier partner created for this company
             rit_2 = rit_obj.browse(cr, uid, rit_obj.search(
                 cr, uid, [('customer_company_id', '=', rc.id)],
                 context=context), context=context)
-            customer_partner_ids = [
-                x.customer_partner_id.id for x in rit_2]
-            # Update all the partner with updated information of the company
-            data = rit_obj._prepare_partner_from_company(
-                cr, uid, rc.id, context=context)
-            rp_obj.write(
-                cr, uid,
-                list(set(customer_partner_ids + supplier_partner_ids)),
-                data, context=context)
+
+            for rit in rit_2:
+                # Update all the partner with updated information
+                data = rit_obj._prepare_partner_from_company(
+                    cr, uid, rc.id, rit.supplier_company_id.id,
+                    context=context)
+                rp_obj.write(
+                    cr, rit.supplier_user_id.id, [rit.customer_partner_id.id],
+                    data, context=ctx)
         return res
