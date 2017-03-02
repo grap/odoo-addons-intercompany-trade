6707284fa8cc0a29a3050df4c5575c2662681768
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-03-02 14:42:40 +0100

    [WIP]

---
+++ intercompany_trade_base/models/res_company.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp import api, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    @api.multi
+    def write(self, vals):
+        """update partners that are flagged as 'intercompany_trade' and
+           are associated to the companies
+           TODO : check if sudo is necessary.
+           """
+        config_obj = self.pool['intercompany.trade.config']
+
+        res = super(ResCompany, self).write(vals)
+
+        for company in self:
+            # Get customer partner created for this company
+            configs = config_obj.search([
+                ('supplier_company_id', '=', company.id)])
+
+            for config in configs:
+                # Update all the partner with updated information
+                data = config_obj._prepare_partner_from_company(
+                    company.id, config.customer_company_id.id)
+                config.supplier_partner_id.write(data)
+
+            # Get supplier partner created for this company
+            configs = config_obj.search([
+                ('customer_company_id', '=', company.id)])
+
+            for config in configs:
+                # Update all the partner with updated information
+                data = config_obj._prepare_partner_from_company(
+                    company.id, config.supplier_company_id.id)
+                config.customer_partner_id.write(data)
+
+        return res
---
+++ intercompany_trade_base/models/res_partner.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import _, api, fields, models
+from openerp.exceptions import Warning as UserError
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    # Columns section
+    intercompany_trade = fields.Boolean(
+        string='Intercompany Trade', readonly=True,
+        help="Indicate that this partner is a company in Odoo.")
+
+    # Overload Section
+    @api.model
+    def create(self, vals):
+        res = super(ResPartner, self).create(vals)
+        res._check_intercompany_trade_access(vals.keys())
+        return res
+
+    @api.multi
+    def write(self, vals):
+        self._check_intercompany_trade_access(vals.keys())
+        return super(ResPartner, self).write(vals)
+
+    @api.multi
+    def unlink(self):
+        self._check_intercompany_trade_access([0])
+        return super(ResPartner, self).unlink()
+
+    # Custom Section
+    @api.model
+    def _intercompany_tradefields_allowed(self):
+        """Overload this function to allow basic users to change
+        some fields for intercompany trade partner"""
+        return []
+
+    @api.multi
+    def _check_intercompany_trade_access(self, fields):
+        """Restrict access of partner set as intercompany_trade for only
+        'intercompany_trade_manager' users."""
+        if self.env.context.get('ignore_intercompany_trade_check', False):
+            return
+        unallowed_fields =\
+            set(fields) - set(self._intercompany_tradefields_allowed())
+        if not self.env.user.has_group(
+                'intercompany_trade_base.intercompany_trade_manager'):
+            for partner in self:
+                if partner.intercompany_trade and unallowed_fields:
+                    raise UserError(_(
+                        "Error: You have no right to create or"
+                        " update a partner that is set as"
+                        " 'Intercompany Trade'"))
