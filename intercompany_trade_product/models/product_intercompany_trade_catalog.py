e9d82575b2ad85d5728d44027234f3a058c1ceab
Sylvain LE GAL <sylvain.legal@grap.coop>
2017-03-07 14:18:32 +0100

    [WIP]

--- intercompany_trade_product/models/product_intercompany_trade_catalog.py
+++ intercompany_trade_product/models/product_intercompany_trade_catalog.py
@@ -2,10 +2,6 @@
 # Copyright (C) 2014 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-# -*- coding: utf-8 -*-
-# Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
-# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import api, fields, models, tools
 from openerp.addons import decimal_precision as dp
