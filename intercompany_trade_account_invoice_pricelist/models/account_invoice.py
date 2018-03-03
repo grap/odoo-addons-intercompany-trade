fd1161d5abf91ac2f9fafaf78a66f0a57b4de69b
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-03-03 12:58:14 +0100

    [ADD] new module for (account_)invoice_pricelist

---
+++ intercompany_trade_account_invoice_pricelist/models/account_invoice.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    @api.multi
+    def prepare_intercompany_invoice(self, config, operation):
+        self.ensure_one()
+        values, other_user = super(
+            AccountInvoice, self).prepare_intercompany_invoice(
+                config, operation)
+        if self.type == 'out_invoice':
+            pricelist = config.sudo(
+                user=other_user).supplier_partner_id.\
+                    property_product_pricelist_purchase
+        elif self.type == 'in_invoice':
+            pricelist = config.sudo(
+                user=other_user).customer_partner_id.property_product_pricelist
+
+        values['pricelist_id'] = pricelist.id
+        return values, other_user
