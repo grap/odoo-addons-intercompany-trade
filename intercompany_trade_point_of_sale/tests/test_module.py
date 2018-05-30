ee1873f0f50dd88d76d7bb9cff392262050d5ca3
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-05-30 17:05:44 +0200

    [REF] rename vars

--- intercompany_trade_point_of_sale/tests/test_module.py
+++ intercompany_trade_point_of_sale/tests/test_module.py
@@ -13,8 +13,8 @@ class Test(TransactionCase):
     def setUp(self):
         super(Test, self).setUp()
 
-        self.pos_session_obj = self.env['pos.session']
-        self.pos_order_obj = self.env['pos.order']
+        self.session_obj = self.env['pos.session']
+        self.order_obj = self.env['pos.order']
         self.intercompany_trade = self.env.ref(
             'intercompany_trade_base.intercompany_trade')
         self.main_config = self.env.ref('point_of_sale.pos_config_main')
@@ -23,12 +23,12 @@ class Test(TransactionCase):
     def test_01_pos_order_constrains(self):
         """[Functional Test] Check if creating a pos order with integrated
         Trade is blocked"""
-        self.session = self.pos_session_obj.create(
+        self.session = self.session_obj.create(
             {'config_id': self.main_config.id})
         self.session.open_cb()
         self.partner = self.intercompany_trade.customer_partner_id
         with self.assertRaises(ValidationError):
-            self.pos_order_obj.create({
+            self.order_obj.create({
                 'session_id': self.session.id,
                 'partner_id': self.partner.id,
             })
