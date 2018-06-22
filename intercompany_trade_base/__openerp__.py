220a918c38f9df635e63f90987b1ca8f3e08a88c
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-06-22 14:06:06 +0200

    [REF] finish intercompany_trade_base

--- intercompany_trade_base/__openerp__.py
+++ intercompany_trade_base/__openerp__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 # Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Intercompany Trade - Base',
-    'version': '8.0.1.0.0',
+    'version': '8.0.2.0.0',
     'category': 'Intercompany Trade',
     'author': 'GRAP',
     'website': 'http://www.grap.coop',
@@ -17,13 +17,9 @@
         'security/ir_module_category.xml',
         'security/res_groups.xml',
         'security/ir.model.access.csv',
-        'views/res_partner_view.xml',
-        'views/intercompany_trade_config_view.xml',
-        'views/action.xml',
         'views/menu.xml',
-    ],
-    'css': [
-        'static/src/css/itb.css',
+        'views/view_res_partner.xml',
+        'views/view_intercompany_trade_config.xml',
     ],
     'demo': [
         'demo/res_company.xml',
