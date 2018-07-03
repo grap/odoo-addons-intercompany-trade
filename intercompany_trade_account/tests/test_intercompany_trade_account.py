36d1dda7fd0827d12ce8b2fbc057084402dfc5dd
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-07-03 13:13:59 +0200

    [WIP]

--- intercompany_trade_account/tests/test_intercompany_trade_account.py
+++ intercompany_trade_account/tests/test_intercompany_trade_account.py
@@ -52,60 +52,60 @@ class Test(TransactionCase):
         self.customer_account_payable = self.env.ref(
             'intercompany_trade_account.customer_account_payable')
 
-####    def test_01_vat_association_bad(self):
-####        """
-####            [Functional Test] Associate products with incompatible VAT
-####            must fail
-####        """
-####        # Associate with bad VAT
-####        # (Customer Service VAT 10% EXCLUDED - Supplier Service VAT 25%)
-####        catalog = self.catalog_obj.sudo(self.customer_user).search([(
-####            'supplier_product_id', '=',
-####            self.product_supplier_service_25_incl.id)])
+    def test_01_vat_association_bad(self):
+        """
+            [Functional Test] Associate products with incompatible VAT
+            must fail
+        """
+        # Associate with bad VAT
+        # (Customer Service VAT 10% EXCLUDED - Supplier Service VAT 25%)
+        catalog = self.catalog_obj.sudo(self.customer_user).search([(
+            'supplier_product_id', '=',
+            self.product_supplier_service_25_incl.id)])
 
-####        link = self.link_obj.with_context(active_id=catalog.id).sudo(
-####            self.customer_user).create({
-####                'customer_product_id':
-####                self.product_customer_service_10_excl.id})
-####        with self.assertRaises(UserError):
-####            link.sudo(self.customer_user).link_product()
+        link = self.link_obj.with_context(active_id=catalog.id).sudo(
+            self.customer_user).create({
+                'customer_product_id':
+                self.product_customer_service_10_excl.id})
+        with self.assertRaises(UserError):
+            link.sudo(self.customer_user).link_product()
 
-####        # Associate with bad VAT
-####        # (Customer Product with no VAT -> Supplier Service VAT 25%)
-####        catalog = self.catalog_obj.sudo(self.customer_user).search([(
-####            'supplier_product_id', '=',
-####            self.product_supplier_service_25_incl.id)])
+        # Associate with bad VAT
+        # (Customer Product with no VAT -> Supplier Service VAT 25%)
+        catalog = self.catalog_obj.sudo(self.customer_user).search([(
+            'supplier_product_id', '=',
+            self.product_supplier_service_25_incl.id)])
 
-####        link = self.link_obj.with_context(active_id=catalog.id).sudo(
-####            self.customer_user).create({
-####                'customer_product_id': self.product_customer_apple.id})
-####        with self.assertRaises(UserError):
-####            link.sudo(self.customer_user).link_product()
+        link = self.link_obj.with_context(active_id=catalog.id).sudo(
+            self.customer_user).create({
+                'customer_product_id': self.product_customer_apple.id})
+        with self.assertRaises(UserError):
+            link.sudo(self.customer_user).link_product()
 
-####    def test_02_vat_association_good(self):
-####        """
-####            [Functional Test] Associate products with compatible VAT
-####            must succeed (Incl / excl)
-####        """
-####        cr, uid = self.cr, self.customer_user.id
-####        # Associate with good VAT
-####        # (Customer Service VAT 10% EXCLUDED
-####        # -> Supplier Service VAT 10% INCLUDE)
+    def test_02_vat_association_good(self):
+        """
+            [Functional Test] Associate products with compatible VAT
+            must succeed (Incl / excl)
+        """
+        cr, uid = self.cr, self.customer_user.id
+        # Associate with good VAT
+        # (Customer Service VAT 10% EXCLUDED
+        # -> Supplier Service VAT 10% INCLUDE)
 
-####        catalog = self.catalog_obj.sudo(self.customer_user).search([(
-####            'supplier_product_id', '=',
-####            self.product_supplier_service_10_incl.id)])
+        catalog = self.catalog_obj.sudo(self.customer_user).search([(
+            'supplier_product_id', '=',
+            self.product_supplier_service_10_incl.id)])
 
-####        link = self.link_obj.with_context(active_id=catalog.id).sudo(
-####            self.customer_user).create({
-####                'customer_product_id':
-####                self.product_customer_service_10_excl.id})
-####        try:
-####            link.sudo(self.customer_user).link_product()
-####        except:
-####            self.assertTrue(
-####                "Associate a Customer Product with 10% Excl VAT to  a supplier"
-####                " Product with 10% Incl VAT must succeed.")
+        link = self.link_obj.with_context(active_id=catalog.id).sudo(
+            self.customer_user).create({
+                'customer_product_id':
+                self.product_customer_service_10_excl.id})
+        try:
+            link.sudo(self.customer_user).link_product()
+        except:
+            self.assertTrue(
+                "Associate a Customer Product with 10% Excl VAT to  a supplier"
+                " Product with 10% Incl VAT must succeed.")
 
 
     def test_03_create_invoice_in(self):
@@ -171,53 +171,9 @@ class Test(TransactionCase):
                 customer_invoice_line.
                 intercompany_trade_account_invoice_line_id)
 
-
         self.assertNotEqual(
             supplier_invoice_line.id, False,
             "Create a Invoice Line must create another invoice Line.")
-
-
-        self.assertEqual(
-            supplier_invoice_line.price_unit, supplier_product.list_price,
-            "Create a In Invoice Line must automatically reset the"
-            " price_unit, using the sale price of the supplier.")
-
-####        sup_pp = sup_pp
-####        # self.assertEqual(
-####        #    SUPER_ail_other.price_unit, sup_pp.list_price,
-####        #    """Create a In Invoice Line must automatically reset the"""
-####        #    """ price_unit, using the sale price of the supplier.""")
-
-
-
-#        # Create a Invoice Line
-#        vals = self.ail_obj.default_get(
-#            cr, cus_uid, ['account_id', 'quantity'],
-#            context=context)
-#        vals.update({
-#            'invoice_id': cus_ai_id,
-#            'name': 'TEST',
-#            'product_id': self.product_customer_service_10_excl.id,
-#            'uos_id': self.product_uom_unit.id,
-#        })
-
-#        cus_ail_id = self.ail_obj.create(cr, cus_uid, vals, context=context)
-
-#        # Checks creation of the according Invoice Line
-#        SUPER_ail = self.ail_obj.browse(cr, self.uid, cus_ail_id)
-#        SUPER_ail_other = SUPER_ail.intercompany_trade_account_invoice_line_id
-
-#        sup_ail_id = SUPER_ail.intercompany_trade_account_invoice_line_id.id
-
-#        self.assertNotEqual(
-#            SUPER_ail_other, False,
-#            """Create a Invoice Line must create another invoice Line.""")
-
-#        sup_pp = sup_pp
-#        # self.assertEqual(
-#        #    SUPER_ail_other.price_unit, sup_pp.list_price,
-#        #    """Create a In Invoice Line must automatically reset the"""
-#        #    """ price_unit, using the sale price of the supplier.""")
 
 ####        # Update Invoice Line (change price = must fail)
 ####        with self.assertRaises(UserError):
