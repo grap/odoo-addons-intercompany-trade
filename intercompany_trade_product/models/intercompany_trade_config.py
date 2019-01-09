f1e7f27d26b82ed63af78f797bfbd0253f8a7f13
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-01-09 15:41:44 +0100

    [REF-IMP] remove useless code. rename function. Introduce possibility to return customer product from supplier one, based on rule, like category

--- intercompany_trade_product/models/intercompany_trade_config.py
+++ intercompany_trade_product/models/intercompany_trade_config.py
@@ -58,57 +58,61 @@ class IntercompanyTradeConfig(models.Model):
         return vals
 
     @api.multi
-    def _get_other_product_info(self, product_id, direction):
+    def _get_product_in_customer_company(self, product):
         """
-            Deliver a product id from another product id.
-            Usefull to call when create (sale / purchase / invoice) line to
-            create according line with correct product;
-
+            Return the product in the customer company from a product in the
+            supplier company
             Realize correct check if the product is not referenced.
 
-            :param @product_id: id of a product.product
-                Current product, added by the customer / the supplier.
-            :param @direction: 'in' / 'out'.
-                'in': for a 'purchase' / 'In Invoice';
-                'out': for a 'sale' / 'Out Invoice';
-
-            :return : {
-                'product_id': xxx;
-                'price_unit': xxx; (only if direction is in);
-            }
+            :param @product: product in the supplier company
+            :return : product, in the customer company
         """
         self.ensure_one()
-        res = {}
+        customer_product = self._get_product_in_customer_company_strict(
+            product)
+        if not customer_product:
+            customer_product =\
+                self._get_product_in_customer_company_approximate(product)
+        if not customer_product:
+            raise UserError(_(
+                "You can not add the product '%s' to the"
+                " current Order or Invoice because the customer didn't"
+                " referenced your product. Please contact him and"
+                " say him to do it.") % (product.name))
+        return customer_product
+
+    @api.multi
+    def _get_product_in_customer_company_strict(self, product):
+        self.ensure_one()
 
         product_obj = self.env['product.product']
         supplierinfo_obj = self.env['product.supplierinfo']
 
         # Get current Product
-        product = product_obj.sudo().browse(product_id)
-        if direction == 'out':
-            supplierinfos = supplierinfo_obj.sudo().search([
-                ('supplier_product_id', '=', product_id),
-                ('name', '=', self.supplier_partner_id.id),
-                ('company_id', '=', self.customer_company_id.id)])
-            if len(supplierinfos) == 0:
-                raise UserError(_(
-                    "You can not add the product '%s' to the"
-                    " current Order or Invoice because the customer didn't"
-                    " referenced your product. Please contact him and"
-                    " say him to do it.") % (product.name))
-            supplierinfo = supplierinfos[0]
-            customer_products = product_obj.sudo().with_context(
-                active_test=False).search([
-                    ('company_id', '=', self.customer_company_id.id),
-                    ('product_tmpl_id', '=', supplierinfo.product_tmpl_id.id),
-                ])
-            if len(customer_products) != 1:
-                raise UserError(_(
-                    "You can not add '%s' to the current Order or Invoice"
-                    " because the customer referenced many variants of"
-                    " this template.") % (
-                        product.name))
-            res['product_id'] = customer_products[0].id
-        else:
-            raise UserError(_("Unimplemented Feature"))
-        return res
+        product = product_obj.sudo().browse(product.id)
+        supplierinfos = supplierinfo_obj.sudo().search([
+            ('supplier_product_id', '=', product.id),
+            ('name', '=', self.supplier_partner_id.id),
+            ('company_id', '=', self.customer_company_id.id)])
+        if len(supplierinfos) == 0:
+            return False
+        supplierinfo = supplierinfos[0]
+        customer_products = product_obj.sudo(
+            self.customer_user_id).with_context(active_test=False).search([
+                ('company_id', '=', self.customer_company_id.id),
+                ('product_tmpl_id', '=', supplierinfo.product_tmpl_id.id),
+            ])
+        if len(customer_products) != 1:
+            raise UserError(_(
+                "You can not add '%s' to the current Order or Invoice"
+                " because the customer referenced many variants of"
+                " this template.") % (
+                    product.name))
+        return customer_products[0]
+
+    @api.multi
+    def _get_product_in_customer_company_approximate(self, product):
+        """Overloadable function, allow to return a product if the
+        customer did'nt referenced the supplier product, by category,
+        or other rules."""
+        return False
