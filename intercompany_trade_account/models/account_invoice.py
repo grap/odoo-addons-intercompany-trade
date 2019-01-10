4def3874b5e3d97bee4736d19d95334ed0635bf3
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-01-10 14:51:29 +0100

    WIP

--- intercompany_trade_account/models/account_invoice.py
+++ intercompany_trade_account/models/account_invoice.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import _, api, fields, models
-from openerp.exceptions import Warning as UserError
+# from openerp.exceptions import Warning as UserError
 
 
 class AccountInvoice(models.Model):
@@ -18,9 +18,6 @@ class AccountInvoice(models.Model):
     ]
 
     # Columns Section
-    intercompany_trade_account_invoice_id = fields.Integer(
-        string='Intercompany Trade Account Invoice', readonly=True)
-
     intercompany_trade = fields.Boolean(
         string='Intercompany Trade', related='partner_id.intercompany_trade',
         store=True)
@@ -29,6 +26,7 @@ class AccountInvoice(models.Model):
         string='Intercompany Trade Readonly',
         compute='_compute_intercompany_trade_readonly')
 
+    # required ?
     amount_total = fields.Float(compute_sudo=True)
 
     amount_tax = fields.Float(compute_sudo=True)
@@ -45,168 +43,169 @@ class AccountInvoice(models.Model):
             invoice.intercompany_trade_readonly = True
 
     # Overload Section
-    @api.model
-    def create(self, vals):
-        partner_obj = self.env['res.partner']
-        partner = partner_obj.browse(vals['partner_id'])
+    # @api.model
+    # def create(self, vals):
+    #     partner_obj = self.env['res.partner']
+    #     partner = partner_obj.browse(vals['partner_id'])
 
-        create_account_invoice = (not self.env.context.get(
-            'intercompany_trade_do_not_propagate', False) and
-            partner.intercompany_trade)
+    #     create_account_invoice = (not self.env.context.get(
+    #         'intercompany_trade_do_not_propagate', False) and
+    #         partner.intercompany_trade)
 
-        if create_account_invoice:
-            line_ids = vals.get('invoice_line', False)
-            vals.pop('invoice_line', None)
+    #     if create_account_invoice:
+    #         line_ids = vals.get('invoice_line', False)
+    #         vals.pop('invoice_line', None)
 
-        invoice = super(AccountInvoice, self).create(vals)
+    #     invoice = super(AccountInvoice, self).create(vals)
 
-        if create_account_invoice and\
-                invoice.type in ['in_invoice', 'in_refund']:
-            raise UserError(_(
-                "You can not create a Purchase invoice or refund for "
-                " intercompany trade. Ask to your supplier to create it."))
+    #     if create_account_invoice and\
+    #             invoice.type in ['in_invoice', 'in_refund']:
+    #         raise UserError(_(
+    #             "You can not create a Purchase invoice or refund for "
+    #             " intercompany trade. Ask to your supplier to create it."))
 
-        if create_account_invoice:
-            # Get config
-            config = self._get_intercompany_trade_by_partner_company_type(
-                invoice.partner_id.id, invoice.company_id.id, invoice.type)
+    #     if create_account_invoice:
+    #         # Get config
+    #         config = self._get_intercompany_trade_by_partner_company_type(
+    #             invoice.partner_id.id, invoice.company_id.id, invoice.type)
 
-            # Create associated Invoice
-            invoice_other_vals, other_user =\
-                invoice.prepare_intercompany_invoice(config, 'create')
+    #         # Create associated Invoice
+    #         invoice_other_vals, other_user =\
+    #             invoice.prepare_intercompany_invoice(config, 'create')
 
-            invoice_other = self.sudo().with_context(
-                intercompany_trade_do_not_propagate=True).create(
-                invoice_other_vals)
+    #         invoice_other = self.sudo().with_context(
+    #             intercompany_trade_do_not_propagate=True).create(
+    #             invoice_other_vals)
 
-            # Set other id
-            invoice.write({
-                'intercompany_trade_account_invoice_id': invoice_other.id,
-            })
+    #         # Set other id
+    #         invoice.write({
+    #             'intercompany_trade_account_invoice_id': invoice_other.id,
+    #         })
 
-            # Update Proper Account Invoice
-            invoice.write({
-                'invoice_line': line_ids,
-            })
+    #         # Update Proper Account Invoice
+    #         invoice.write({
+    #             'invoice_line': line_ids,
+    #         })
 
-        return invoice
+    #     return invoice
 
     # TODO FORBID state change for customer
     # TODO refactor state management (verify state) or wait for V10
-    @api.multi
-    def write(self, vals):
-        res = super(AccountInvoice, self).write(vals)
+    # @api.multi
+    # def write(self, vals):
+    #     res = super(AccountInvoice, self).write(vals)
 
-        if not self.env.context.get(
-                'intercompany_trade_do_not_propagate', False):
+    #     if not self.env.context.get(
+    #             'intercompany_trade_do_not_propagate', False):
 
-            for invoice in self.filtered(lambda x: x.intercompany_trade):
-                if vals.get('state', False) == 'cancel':
-                    raise UserError(_(
-                        "You can not cancel intercompany Trade. Please make"
-                        " a Refund."))
-                if invoice.type in ['in_invoice', 'in_refund']:
-                    copy_vals = vals.copy()
-                    for key in self._CUSTOMER_ALLOWED_FIELDS:
-                        copy_vals.pop(key, False)
-                    if copy_vals:
-                        raise UserError(_(
-                            "You can not write a Purchase invoice or refund"
-                            " for intercompany trade. Ask to your supplier"
-                            " to create it.\n\n %s") % ', '.join(
-                                [x for x in copy_vals.keys()]))
-                    continue
+    #         for invoice in self.filtered(lambda x: x.intercompany_trade):
+    #             if vals.get('state', False) == 'cancel':
+    #                 raise UserError(_(
+    #                     "You can not cancel intercompany Trade. Please make"
+    #                     " a Refund."))
+    #             if invoice.type in ['in_invoice', 'in_refund']:
+    #                 copy_vals = vals.copy()
+    #                 for key in self._CUSTOMER_ALLOWED_FIELDS:
+    #                     copy_vals.pop(key, False)
+    #                 if copy_vals:
+    #                     raise UserError(_(
+    #                         "You can not write a Purchase invoice or refund"
+    #                         " for intercompany trade. Ask to your supplier"
+    #                         " to create it.\n\n %s") % ', '.join(
+    #                             [x for x in copy_vals.keys()]))
+    #                 continue
 
-                config =\
-                    self._get_intercompany_trade_by_partner_company_type(
-                        invoice.partner_id.id, invoice.company_id.id,
-                        invoice.type)
-                # Disable possibility to change the supplier
-                if 'partner_id' in vals and\
-                        vals.get('partner_id') != invoice.partner_id.id:
-                    raise UserError(_(
-                        "Error!\nYou can not change the partner because of"
-                        " Intercompany Trade Rules. Please create"
-                        " a new Invoice."))
+    #             config =\
+    #                 self._get_intercompany_trade_by_partner_company_type(
+    #                     invoice.partner_id.id, invoice.company_id.id,
+    #                     invoice.type)
+    #             # Disable possibility to change the supplier
+    #             if 'partner_id' in vals and\
+    #                     vals.get('partner_id') != invoice.partner_id.id:
+    #                 raise UserError(_(
+    #                     "Error!\nYou can not change the partner because of"
+    #                     " Intercompany Trade Rules. Please create"
+    #                     " a new Invoice."))
 
-                # Update changes for according invoice
-                invoice_vals, other_user =\
-                    invoice.prepare_intercompany_invoice(config, 'update')
+    #             # Update changes for according invoice
+    #             invoice_vals, other_user =\
+    #                 invoice.prepare_intercompany_invoice(config, 'update')
 
-                invoice_other = invoice.sudo().browse(
-                    invoice.intercompany_trade_account_invoice_id)
-                invoice_other.with_context(
-                    intercompany_trade_do_not_propagate=True).write(
-                        invoice_vals)
+    #             invoice_other = invoice.sudo().browse(
+    #                 invoice.intercompany_trade_account_invoice_id)
+    #             invoice_other.with_context(
+    #                 intercompany_trade_do_not_propagate=True).write(
+    #                     invoice_vals)
 
-                # Check during State changes if amount are coherent
-                if vals.get('state', False):
-                    if invoice_other.amount_untaxed !=\
-                            invoice.amount_untaxed\
-                            or invoice_other.amount_tax !=\
-                            invoice.amount_tax:
-                        raise UserError(_(
-                            "Error!\nYou can not change the state of this"
-                            " invoice because the according customer invoice"
-                            " doesn't have the same total amount."
-                            " Please fix the problem first."))
+    #             # Check during State changes if amount are coherent
+    #             if vals.get('state', False):
+    #                 if invoice_other.amount_untaxed !=\
+    #                         invoice.amount_untaxed\
+    #                         or invoice_other.amount_tax !=\
+    #                         invoice.amount_tax:
+    #                     raise UserError(_(
+    #                         "Error!\nYou can not change the state of this"
+    #                         " invoice because the according customer invoice"
+    #                         " doesn't have the same total amount."
+    #                         " Please fix the problem first."))
 
-                if vals.get('state', False) == 'open':
-                    invoice_other.sudo().signal_workflow('invoice_open')
+    #             if vals.get('state', False) == 'open':
+    #                 invoice_other.sudo().signal_workflow('invoice_open')
 
-        return res
+    #     return res
 
-    @api.multi
-    def unlink(self):
-        if not self.env.context.get(
-                'intercompany_trade_do_not_propagate', False):
+    # @api.multi
+    # def unlink(self):
+    #     if not self.env.context.get(
+    #             'intercompany_trade_do_not_propagate', False):
 
-            for invoice in self.filtered(lambda x: x.intercompany_trade):
-                # Block Customer Deletion
-                if invoice.type in ('in_invoice', 'in_refund'):
-                    raise UserError(_(
-                        "Error!\nYou can not delete invoices."
-                        " Please ask to your supplier to do it."))
+    #         for invoice in self.filtered(lambda x: x.intercompany_trade):
+    #             # Block Customer Deletion
+    #             if invoice.type in ('in_invoice', 'in_refund'):
+    #                 raise UserError(_(
+    #                     "Error!\nYou can not delete invoices."
+    #                     " Please ask to your supplier to do it."))
 
-                # Delete according Invoice
-                invoice_other = invoice.sudo().browse(
-                    invoice.intercompany_trade_account_invoice_id)
-                invoice_other.with_context(
-                    intercompany_trade_do_not_propagate=True).unlink()
+    #             # Delete according Invoice
+    #             invoice_other = invoice.sudo().browse(
+    #                 invoice.intercompany_trade_account_invoice_id)
+    #             invoice_other.with_context(
+    #                 intercompany_trade_do_not_propagate=True).unlink()
 
-        return super(AccountInvoice, self).unlink()
+    #     return super(AccountInvoice, self).unlink()
 
     # Custom Section
-    @api.model
-    def _get_intercompany_trade_by_partner_company_type(
-            self, partner_id, company_id, type):
+    @api.multi
+    def _get_intercompany_trade_config_by_partner_company_type(self):
         config_obj = self.env['intercompany.trade.config']
 
-        if type in ('in', 'in_invoice', 'in_refund'):
+        self.ensure_one()
+        if self.type in ('in', 'in_invoice', 'in_refund'):
             regular_type = 'in'
         else:
             regular_type = 'out'
 
         return config_obj._get_intercompany_trade_by_partner_company(
-            partner_id, company_id, regular_type)
+            self.partner_id.id, self.company_id.id, regular_type)
 
     @api.multi
-    def prepare_intercompany_invoice(self, config, operation):
+    def _prepare_intercompany_invoice_in_customer_company(self):
         self.ensure_one()
+        config = self._get_intercompany_trade_config_by_partner_company_type()
         other_user = config.customer_user_id
         other_company_id = config.customer_company_id.id
         other_partner_id = config.supplier_partner_id.id
+        # A Sale Invoice Create a Purchase Invoice
         if self.type == 'out_invoice':
-            # A Sale Invoice Create a Purchase Invoice
             other_type = 'in_invoice'
         elif self.type == 'out_refund':
             # A Sale Refund Create a Purchase Refund
             other_type = 'in_refund'
 
-        account_info = self.sudo().onchange_partner_id(
+        account_info = self.sudo(other_user).onchange_partner_id(
             other_type, other_partner_id, company_id=other_company_id)['value']
 
-        account_journal = self.sudo().with_context(
+        account_journal = self.sudo(other_user).with_context(
             type=other_type, company_id=other_company_id)._default_journal()
 
         values = {
