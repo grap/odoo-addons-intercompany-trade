e18ee9b0acc12b02eb02043095be2dde7ae1dad6
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-02-25 12:39:21 +0100

    wip

--- intercompany_trade_product/models/__init__.py
+++ intercompany_trade_product/models/__init__.py
@@ -2,6 +2,7 @@
 from . import product_pricelist
 from . import res_partner
 from . import product_supplierinfo
+from . import intercompany_trade_catalog
 from . import intercompany_trade_config
 from . import intercompany_trade_config_line
 # from . import product_intercompany_trade_catalog
--- intercompany_trade_product/models/product_supplierinfo.py
+++ intercompany_trade_product/models/product_supplierinfo.py
@@ -11,43 +11,84 @@ class ProductSupplierinfo(models.Model):
     _inherit = 'product.supplierinfo'
 
     # Columns Section
-    # intercompany_trade_price = fields.Float(
-    #     string='Unit Price', compute='_compute_intercompany_trade_price',
-    #     store=True,
-    #     digits_compute=dp.get_precision('Intercompany Trade Product Price'))
+    is_intercompany_trade = fields.Boolean(
+        compute='_compute_is_intercompany_trade', store=True)
+
+    intercompany_trade_id = fields.Many2one(
+        string='Intercompany Trade', store=True,
+        compute='_compute_intercompany_trade_id',
+        comodel_name='intercompany.trade.config')
 
     supplier_product_id = fields.Many2one(
         comodel_name='product.product', string='Supplier Product',
         selected=True, _prefetch=False)
 
-#    readonly=True,
+    catalog_id = fields.Many2one(
+        comodel_name='intercompany.trade.catalog', string='Catalog',
+        compute='_compute_catalog_id', inverse='_set_catalog_id',
+        domain="[('intercompany_trade_id', '=', intercompany_trade_id)]")
 
     # Compute Section
-    # @api.multi
-    # @api.depends('supplier_product_id', 'pricelist_ids')
-    # def _compute_intercompany_trade_price(self):
-    #     for supplierinfo in self:
-    #         if supplierinfo.supplier_product_id and
-    # supplierinfo.pricelist_ids:
-    #             supplierinfo.intercompany_trade_price =\
-    #                 supplierinfo.pricelist_ids[0].price
-    #         else:
-    #             supplierinfo.intercompany_trade_price = 0
+    @api.multi
+    @api.depends('name')
+    def _compute_is_intercompany_trade(self):
+        for supplierinfo in self.filtered(lambda x: x.name):
+            supplierinfo.is_intercompany_trade =\
+                supplierinfo.name.intercompany_trade
+
+    @api.multi
+    @api.depends('name')
+    def _compute_intercompany_trade_id(self):
+        IntercompanyTradeConfig = self.env['intercompany.trade.config']
+        for supplierinfo in self.filtered(lambda x: x.name):
+            supplierinfo.intercompany_trade_id =\
+                IntercompanyTradeConfig.search([
+                    ('customer_company_id', '=', supplierinfo.company_id.id),
+                    ('supplier_partner_id', '=', supplierinfo.name.id)]).id
+
+    @api.multi
+    @api.depends('intercompany_trade_id', 'supplier_product_id')
+    def _compute_catalog_id(self):
+        print "_compute_catalog_id"
+        for supplierinfo in self.filtered(
+                lambda x: x.intercompany_trade_id and x.supplier_product_id):
+            supplierinfo.catalog_id = int(
+                '%s%s' % (
+                    supplierinfo.supplier_product_id.id,
+                    str(supplierinfo.intercompany_trade_id.id).rjust(4, '0')))
+
+    @api.multi
+    def _set_catalog_id(self):
+        print "_set_catalog_id"
+        for supplierinfo in self.filtered(lambda x: x.catalog_id):
+            res =\
+                int(str(supplierinfo.catalog_id.id)[:-4])
+            print res
+            supplierinfo.supplier_product_id = res
+
+    @api.onchange('catalog_id')
+    def _onchange_catalog_id(self):
+        print "_onchange_catalog_id"
+        for supplierinfo in self.filtered(lambda x: x.catalog_id):
+            res =\
+                int(str(supplierinfo.catalog_id.id)[:-4])
+            print res
+            supplierinfo.supplier_product_id = res
 
     # Constrains Section
     @api.multi
-    @api.constrains('supplier_product_id', 'name')
+    @api.constrains('supplier_product_id', 'is_intercompany_trade')
     def _check_supplier_product_id(self):
         for supplierinfo in self.filtered(lambda x: x.supplier_product_id):
-            if not supplierinfo.name.intercompany_trade:
+            if not supplierinfo.is_intercompany_trade:
                 raise ValidationError(_(
                     "You can only set Intercompany Trade product with"
                     " customer that are flagged as Intercompany Trade"
                     " Supplier"))
 
         for supplierinfo in self.filtered(lambda x: not x.supplier_product_id):
-            if supplierinfo.name.intercompany_trade:
+            if supplierinfo.is_intercompany_trade:
                 raise ValidationError(_(
                     "You have selected a supplier flagged as Intercompany"
                     " Trade. You should select a Intercompany Trade Product"
-                    "via the interface"))
+                    " via the search field"))
