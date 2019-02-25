df191a8865164ee0c12637497b18420788124dc3
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-02-25 16:17:22 +0100

    wip

--- intercompany_trade_account/models/__init__.py
+++ intercompany_trade_account/models/__init__.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 from . import product_pricelist
-# from . import product_intercompany_trade_catalog
 # from . import intercompany_trade_config
 # from . import intercompany_trade_wizard_link_product
 from . import account_invoice
