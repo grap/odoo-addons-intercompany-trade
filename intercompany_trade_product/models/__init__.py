4a39df0fb75f928b7a703763d069fdf5e3482501
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-02-26 11:00:50 +0100

    wip

--- intercompany_trade_product/models/__init__.py
+++ intercompany_trade_product/models/__init__.py
@@ -1,8 +1,6 @@
 # coding: utf-8
-from . import product_pricelist
-from . import res_partner
-from . import product_supplierinfo
 from . import intercompany_trade_catalog
 from . import intercompany_trade_config
 from . import intercompany_trade_config_line
-# from . import intercompany_trade_wizard_link_product
+from . import product_supplierinfo
+from . import res_partner
--- intercompany_trade_product/models/product_supplierinfo.py
+++ intercompany_trade_product/models/product_supplierinfo.py
@@ -3,7 +3,8 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from openerp import _, api, fields, models
+from openerp.exceptions import Warning as UserError
 
 
 class ProductSupplierinfo(models.Model):
@@ -67,3 +68,84 @@ class ProductSupplierinfo(models.Model):
         for supplierinfo in self.filtered(lambda x: x.catalog_id):
             res = int(str(supplierinfo.catalog_id.id)[:-4])
             supplierinfo.supplier_product_id = res
+
+    @api.constrains(
+        'supplier_product_id', 'is_intercompany_trade', 'product_tmpl_id')
+    def _check_intercompany_trade(self):
+        for supplierinfo in self.filtered(lambda x: x.is_intercompany_trade):
+            # Check if the supplier product has been linked to other
+            # product
+            res = self.search([
+                ('supplier_product_id', '=',
+                    supplierinfo.supplier_product_id.id),
+                ('id', '!=', supplierinfo.id)
+            ])
+            if len(res):
+                raise UserError(_(
+                    "The product(s) %s  are still linked to the"
+                    " the supplier product. You can not link the product"
+                    " %s.") % (
+                        ', '.join(res.mapped('product_tmpl_id.name')),
+                        supplierinfo.product_tmpl_id.name,
+                ))
+
+    # TODO TODO TODO
+    # ADD CHECK
+    # @api.multi
+    # def link_product(self):
+    #     supplierinfo_obj = self.env['product.supplierinfo']
+    #     template_obj = self.env['product.template']
+    #     product_obj = self.env['product.product']
+    #     catalog_obj = self.env['product.intercompany.trade.catalog']
+
+    #     self.ensure_one()
+    #     # Prepare Product Supplierinfo
+    #     supplierinfo_vals =\
+    #         self.intercompany_trade_id._prepare_product_supplierinfo(
+    #             self.supplier_product_id.id, self.customer_product_id.id)
+    #     supplierinfo_vals['product_tmpl_id'] =\
+    #       self.customer_product_tmpl_id.id
+
+    #     cus_template = template_obj.browse(
+    #         supplierinfo_vals['product_tmpl_id'])
+    #     sup_product = product_obj.sudo().browse(
+    #         supplierinfo_vals['supplier_product_id'])
+
+    #     # Raise error if there is many products associated to the template
+    #     product_qty = product_obj.search([
+    #         ('product_tmpl_id', '=', self.customer_product_tmpl_id.id)])
+    #     if len(product_qty) != 1:
+    #         raise UserError(_(
+    #             "Too Many Variants for the Product !\nYou can not link this"
+    #             " product %s because there are %d Variants associated.") % (
+    #                 cus_template.name, len(product_qty)))
+
+    #     # raise error if there is a product linked
+    #     catalogs = catalog_obj.search([
+    #         ('customer_product_tmpl_id', '=',
+    #             supplierinfo_vals['product_tmpl_id']),
+    #         ('customer_company_id', '=', supplierinfo_vals['company_id'])])
+    #     if len(catalogs) != 0:
+    #         if not self.env.context.get('demo_integrated', False):
+    #             raise UserError(_(
+    #                 "Duplicated References !\nYou can not link the Product"
+    #                 " %s because it is yet linked to another supplier"
+    # "product."
+    #                 " Please unlink the Product and try again.") % (
+    #                     cus_template.name))
+    #         else:
+    #             return
+
+    #     # Raise an error if Unit doesn't match
+    #     if cus_template.uom_id.category_id.id !=\
+    #             sup_product.uom_id.category_id.id:
+    #         raise UserError(_(
+    #             "Unit Mismatch !\n The type of Unit of Mesure of your"
+    # "" product"
+    #             " is '%s'.\nThe type of Unit of Mesure of the supplier"
+    # "" product"
+    #             " is '%s'.\n\nThe type must be the same.") % (
+    #                 cus_template.uom_id.category_id.name,
+    #                 sup_product.uom_id.category_id.name))
+
+    #     supplierinfo_obj.create(supplierinfo_vals)
