70c85278c0d2690f17b87aed552e1b5da8e65451
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-03-04 12:40:33 +0100

    wip

--- intercompany_trade_fiscal_company/tests/test_module.py
+++ intercompany_trade_fiscal_company/tests/test_module.py
@@ -7,10 +7,6 @@ from openerp.addons.intercompany_trade_product.tests.\
     test_module import\
     TestModule as TestIntercompanyTradeProduct
 
-from openerp.addons.intercompany_trade_stock_account.tests.\
-    test_module import\
-    TestModule as TestIntercompanyTradeStockAccount
-
 from openerp.addons.intercompany_trade_base.tests.\
     test_module import\
     TestModule as TestIntercompanyTradeBase
@@ -27,29 +23,8 @@ class TestModuleProduct(TestIntercompanyTradeProduct):
     def setUp(self):
         super(TestModuleProduct, self).setUp()
 
-    def test_01_product_association(self):
-        """[Functional Test] Check if associate a product create a
-        product supplierinfo"""
-        self._test_01_product_association()
+    def test_01_product_association_by_product(self):
+        self._test_01_product_association_by_product()
 
-    def test_02_product_association_recovery(self):
-        """
-            - Get supplier product from customer product
-            - Get Customer Product from supplier Product"""
-        self._test_02_product_association_recovery()
-
-    def test_03_create_manual_supplier_info(self):
-        """ Check if create manual supplierinfo fail if partner
-        is flagged as Intercompany Trade."""
-        self._test_03_create_manual_supplier_info()
-
-
-class TestModuleStockAccount(TestIntercompanyTradeStockAccount):
-
-    def setUp(self):
-        super(TestModuleStockAccount, self).setUp()
-
-    def test_01_invoice_stock_picking(self):
-        """[Functional Test] Check if invoicing stock picking
-        create correct intercompany trade invoice"""
-        self._test_01_invoice_stock_picking()
+    def test_02_product_association_by_rule(self):
+        self._test_02_product_association_by_rule()
