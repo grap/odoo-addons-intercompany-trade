5639bbe32e22f08c63b37497a6fc07fb10bdb8f5
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-03-04 12:16:04 +0100

    wip

--- intercompany_trade_account_cancel/models/account_invoice.py
+++ intercompany_trade_account_cancel/models/account_invoice.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
+# Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
