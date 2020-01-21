0e388e8a2fb05bbecd1c082cff0fd209fb87a543
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-21 09:29:20 +0100

    [MIG][12.0] (from 8.0) blackify code

--- intercompany_trade_account/models/account_invoice_line.py
+++ intercompany_trade_account/models/account_invoice_line.py
@@ -1,14 +1,13 @@
-# coding: utf-8
 # Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, api, fields, models
-from openerp.exceptions import Warning as UserError
+from odoo import _, api, fields, models
+from odoo.exceptions import Warning as UserError
 
 
 class AccountInvoiceLine(models.Model):
-    _inherit = 'account.invoice.line'
+    _inherit = "account.invoice.line"
 
     # Due to bad design, some field are written on computed function
     # with account_invoice_triple_discount
@@ -20,8 +19,10 @@ class AccountInvoiceLine(models.Model):
 
     # Columns Section
     intercompany_trade = fields.Boolean(
-        string='Intercompany Trade',
-        related='invoice_id.intercompany_trade', store=True)
+        string="Intercompany Trade",
+        related="invoice_id.intercompany_trade",
+        store=True,
+    )
 
     # Custom Section
     @api.multi
@@ -30,33 +31,41 @@ class AccountInvoiceLine(models.Model):
         invoice = self.invoice_id
 
         # Create according account invoice line
-        customer_product = config.get_customer_product(
-            self.product_id)
+        customer_product = config.get_customer_product(self.product_id)
 
         if not customer_product:
-            raise UserError(_(
-                "It is not possible to confirm this invoice, because"
-                " your customer didn't referenced your product %s-%s") % (
-                self.product_id.code, self.product_id.name))
+            raise UserError(
+                _(
+                    "It is not possible to confirm this invoice, because"
+                    " your customer didn't referenced your product %s-%s"
+                )
+                % (self.product_id.code, self.product_id.name)
+            )
 
         values = self.sudo(config.customer_user_id).product_id_change(
             customer_product.id,
-            False, type=invoice.type.replace('out_', 'in_'),
+            False,
+            type=invoice.type.replace("out_", "in_"),
             company_id=config.customer_company_id.id,
             partner_id=config.supplier_partner_id.id,
-            fposition_id=invoice.fiscal_position.id)['value']
+            fposition_id=invoice.fiscal_position.id,
+        )["value"]
 
-        values.update({
-            'invoice_id': customer_invoice.id,
-            'product_id': customer_product.id,
-            'company_id': customer_invoice.company_id.id,
-            'partner_id': customer_invoice.partner_id.id,
-            'quantity': self.quantity,
-            'price_unit': self.price_unit,
-            'discount': self.discount,
-            'uos_id': self.uos_id.id,
-            'invoice_line_tax_id': (
-                values.get('invoice_line_tax_id', False) and
-                [[6, False, values['invoice_line_tax_id']]] or False),
-        })
+        values.update(
+            {
+                "invoice_id": customer_invoice.id,
+                "product_id": customer_product.id,
+                "company_id": customer_invoice.company_id.id,
+                "partner_id": customer_invoice.partner_id.id,
+                "quantity": self.quantity,
+                "price_unit": self.price_unit,
+                "discount": self.discount,
+                "uos_id": self.uos_id.id,
+                "invoice_line_tax_id": (
+                    values.get("invoice_line_tax_id", False)
+                    and [[6, False, values["invoice_line_tax_id"]]]
+                    or False
+                ),
+            }
+        )
         return values
