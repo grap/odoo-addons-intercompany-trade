93b6aadebbb4505999d7cbc405f75a1da1683ce9
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-21 09:29:20 +0100

    [MIG][12.0] (from 8.0) blackify code

--- intercompany_trade_product/tests/test_module.py
+++ intercompany_trade_product/tests/test_module.py
@@ -1,17 +1,16 @@
-# coding: utf-8
 # Copyright (C) 2014 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
 
-# from openerp.exceptions import Warning as UserError
-from openerp.exceptions import ValidationError
-from openerp.tests.common import TransactionCase
+# from odoo.exceptions import Warning as UserError
+from odoo.exceptions import ValidationError
+from odoo.tests.common import TransactionCase
 
-from openerp.addons.intercompany_trade_base.tests.\
-    test_module import\
-    TestModule as TestIntercompanyTradeBase
+from odoo.addons.intercompany_trade_base.tests.test_module import (
+    TestModule as TestIntercompanyTradeBase,
+)
 
 
 class TestBase(TestIntercompanyTradeBase):
@@ -30,39 +29,50 @@ class TestModule(TransactionCase):
         super(TestModule, self).setUp()
 
         # Get Registries
-        self.IrModuleModule = self.env['ir.module.module']
-        self.ProductSupplierinfo = self.env['product.supplierinfo']
+        self.IrModuleModule = self.env["ir.module.module"]
+        self.ProductSupplierinfo = self.env["product.supplierinfo"]
 
         # Get objects from xml_ids
         self.config = self.env.ref(
-            'intercompany_trade_base.intercompany_trade')
+            "intercompany_trade_base.intercompany_trade"
+        )
         self.config_line_category = self.env.ref(
-            'intercompany_trade_product.it_line')
+            "intercompany_trade_product.it_line"
+        )
 
-        self.apple_category = self.env.ref('product.apple')
+        self.apple_category = self.env.ref("product.apple")
 
         self.supplier_banana = self.env.ref(
-            'intercompany_trade_product.product_supplier_banana')
+            "intercompany_trade_product.product_supplier_banana"
+        )
 
         self.supplier_apple = self.env.ref(
-            'intercompany_trade_product.product_supplier_apple')
+            "intercompany_trade_product.product_supplier_apple"
+        )
         self.customer_apple = self.env.ref(
-            'intercompany_trade_product.product_customer_apple')
+            "intercompany_trade_product.product_customer_apple"
+        )
 
         self.supplier_service = self.env.ref(
-            'intercompany_trade_product.product_supplier_service')
+            "intercompany_trade_product.product_supplier_service"
+        )
         self.customer_service = self.env.ref(
-            'intercompany_trade_product.product_customer_service')
+            "intercompany_trade_product.product_customer_service"
+        )
 
         self.supplier_imac = self.env.ref(
-            'intercompany_trade_product.product_supplier_imac_computer')
+            "intercompany_trade_product.product_supplier_imac_computer"
+        )
         self.customer_it_raws = self.env.ref(
-            'intercompany_trade_product.product_customer_it_raws')
+            "intercompany_trade_product.product_customer_it_raws"
+        )
 
         self.customer_user = self.env.ref(
-            'intercompany_trade_base.customer_user')
+            "intercompany_trade_base.customer_user"
+        )
         self.supplier_user = self.env.ref(
-            'intercompany_trade_base.supplier_user')
+            "intercompany_trade_base.supplier_user"
+        )
 
     def test_01_product_association_by_product(self):
         # this test creates pricelist.partnerinfo
@@ -72,42 +82,53 @@ class TestModule(TransactionCase):
         # in openerp/osv/expression.py #830
         # to avoid this bug, we prevent running this this if
         # product_fiscal_company is installed.
-        if not self.IrModuleModule.search([
-                ('name', '=', 'product_fiscal_company'),
-                ('state', '=', 'installed')]):
+        if not self.IrModuleModule.search(
+            [
+                ("name", "=", "product_fiscal_company"),
+                ("state", "=", "installed"),
+            ]
+        ):
             self._test_01_product_association_by_product()
         else:
             _logger.info(
                 "test skipped, will be run later in"
-                " intercompany_trade_fiscal_company")
+                " intercompany_trade_fiscal_company"
+            )
 
     def test_02_product_association_by_rule(self):
         # Same reason here.
-        if not self.IrModuleModule.search([
-                ('name', '=', 'product_fiscal_company'),
-                ('state', '=', 'installed')]):
+        if not self.IrModuleModule.search(
+            [
+                ("name", "=", "product_fiscal_company"),
+                ("state", "=", "installed"),
+            ]
+        ):
             self._test_02_product_association_by_rule()
         else:
             _logger.info(
                 "test skipped, will be run later in"
-                " intercompany_trade_fiscal_company")
+                " intercompany_trade_fiscal_company"
+            )
 
     # Test Section
     def _test_01_product_association_by_product(self):
         # Test if getting the product in the supplier context works.
         customer_product = self.config.sudo(
-            self.supplier_user).get_customer_product(self.supplier_apple)
+            self.supplier_user
+        ).get_customer_product(self.supplier_apple)
 
         self.assertEqual(
-            customer_product, self.customer_apple,
-            "Recovering product in the supplier context failed")
+            customer_product,
+            self.customer_apple,
+            "Recovering product in the supplier context failed",
+        )
 
         # Try to link to the same customer product to another
         # supplier product, should fail
         vals = {
-            'name': self.config.supplier_partner_id.id,
-            'product_tmpl_id': self.customer_service.product_tmpl_id.id,
-            'supplier_product_id': self.supplier_apple.id,
+            "name": self.config.supplier_partner_id.id,
+            "product_tmpl_id": self.customer_service.product_tmpl_id.id,
+            "supplier_product_id": self.supplier_apple.id,
         }
 
         with self.assertRaises(ValidationError):
@@ -116,42 +137,55 @@ class TestModule(TransactionCase):
 
         # Test with another product
         customer_product = self.config.sudo(
-            self.supplier_user).get_customer_product(self.supplier_service)
+            self.supplier_user
+        ).get_customer_product(self.supplier_service)
 
         self.assertEqual(
-            customer_product, self.customer_service,
-            "Recovering product in the supplier context failed")
+            customer_product,
+            self.customer_service,
+            "Recovering product in the supplier context failed",
+        )
 
     def _test_02_product_association_by_rule(self):
         # Test if getting the product in the supplier context works.
         # by rule
         customer_product = self.config.sudo(
-            self.supplier_user).get_customer_product(self.supplier_imac)
+            self.supplier_user
+        ).get_customer_product(self.supplier_imac)
 
         self.assertEqual(
-            customer_product, self.customer_it_raws,
-            "Recovering by category rule should succeed. (exact category)")
+            customer_product,
+            self.customer_it_raws,
+            "Recovering by category rule should succeed. (exact category)",
+        )
 
         # Change the rule category for a parent product
         self.config_line_category.categ_id = self.apple_category
 
         customer_product = self.config.sudo(
-            self.supplier_user).get_customer_product(self.supplier_imac)
+            self.supplier_user
+        ).get_customer_product(self.supplier_imac)
 
         self.assertEqual(
-            customer_product, self.customer_it_raws,
-            "Recovering by category rule should succeed. (parent category)")
+            customer_product,
+            self.customer_it_raws,
+            "Recovering by category rule should succeed. (parent category)",
+        )
 
         customer_product = self.config.sudo(
-            self.supplier_user).get_customer_product(self.supplier_banana)
+            self.supplier_user
+        ).get_customer_product(self.supplier_banana)
 
         self.assertEqual(
-            customer_product, False,
-            "Recovering by incorrect category rule should not return product")
+            customer_product,
+            False,
+            "Recovering by incorrect category rule should not return product",
+        )
 
         # Set no category for the rule
         self.config_line_category.categ_id = False
         self.config_line_category.categ_id = self.apple_category
 
         customer_product = self.config.sudo(
-            self.supplier_user).get_customer_product(self.supplier_imac)
+            self.supplier_user
+        ).get_customer_product(self.supplier_imac)
