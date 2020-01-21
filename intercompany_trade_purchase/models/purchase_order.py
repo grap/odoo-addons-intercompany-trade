e8dfc70a746d6772901e4dbc0455b1df88143235
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-21 09:29:20 +0100

    [MIG][12.0] (from 8.0) blackify code

--- intercompany_trade_purchase/models/purchase_order.py
+++ intercompany_trade_purchase/models/purchase_order.py
@@ -1,17 +1,17 @@
-# coding: utf-8
 # Copyright (C) 2014 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models, fields, _
-from openerp.exceptions import Warning as UserError
+from odoo import api, models, fields, _
+from odoo.exceptions import Warning as UserError
 
 
 class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
+    _inherit = "purchase.order"
 
     intercompany_trade = fields.Boolean(
-        string='Intercompany Trade', related='partner_id.intercompany_trade')
+        string="Intercompany Trade", related="partner_id.intercompany_trade"
+    )
 
     @api.multi
     def action_invoice_create(self):
@@ -22,17 +22,22 @@ class PurchaseOrder(models.Model):
     def view_invoice(self):
         orders = self.filtered(lambda x: x.intercompany_trade)
         if orders:
-            raise UserError(_(
-                "In Intercompany Trade context, The supplier invoices will"
-                " be created by your supplier"))
+            raise UserError(
+                _(
+                    "In Intercompany Trade context, The supplier invoices will"
+                    " be created by your supplier"
+                )
+            )
         return self.view_invoice()
 
     @api.model
     def _prepare_order_line_move(
-            self, order, order_line, picking_id, group_id):
+        self, order, order_line, picking_id, group_id
+    ):
         res = super(PurchaseOrder, self)._prepare_order_line_move(
-            order, order_line, picking_id, group_id)
+            order, order_line, picking_id, group_id
+        )
         if order.intercompany_trade:
             for item in res:
-                item['invoice_state'] = 'none'
+                item["invoice_state"] = "none"
         return res
