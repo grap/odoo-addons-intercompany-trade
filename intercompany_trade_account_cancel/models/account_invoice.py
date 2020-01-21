4fd5107548a6f47d332d82a404cc68503f689795
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-21 09:29:20 +0100

    [MIG][12.0] (from 8.0) blackify code

--- intercompany_trade_account_cancel/models/account_invoice.py
+++ intercompany_trade_account_cancel/models/account_invoice.py
@@ -1,19 +1,17 @@
-# coding: utf-8
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, api, models
-from openerp.exceptions import Warning as UserError
+from odoo import _, api, models
+from odoo.exceptions import Warning as UserError
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     @api.multi
     def action_cancel(self):
         invoices = self.filtered(lambda x: x.intercompany_trade)
         if invoices:
-            raise UserError(_(
-                "Unable to cancel intercompany trade Invoices"))
+            raise UserError(_("Unable to cancel intercompany trade Invoices"))
         return super(AccountInvoice, self).action_cancel()
