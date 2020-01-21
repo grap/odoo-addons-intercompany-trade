1301a16dedadf9e9ac730607c31d61f032a98948
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-01-21 09:29:20 +0100

    [MIG][12.0] (from 8.0) blackify code

--- intercompany_trade_point_of_sale/models/pos_order.py
+++ intercompany_trade_point_of_sale/models/pos_order.py
@@ -1,21 +1,23 @@
-# coding: utf-8
 # Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, api, models
-from openerp.exceptions import ValidationError
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
 
 
 class PosOrder(models.Model):
-    _inherit = 'pos.order'
+    _inherit = "pos.order"
 
     @api.multi
-    @api.constrains('partner_id')
+    @api.constrains("partner_id")
     def _check_partner_integrated_trade(self):
         for order in self:
             if order.partner_id.intercompany_trade:
-                raise ValidationError(_(
-                    "You can not select a partner marked as 'integrated Trade'"
-                    " in a Point of sale context. Please use the sale or"
-                    " the invoice module instead."))
+                raise ValidationError(
+                    _(
+                        "You can not select a partner marked as 'integrated Trade'"
+                        " in a Point of sale context. Please use the sale or"
+                        " the invoice module instead."
+                    )
+                )
