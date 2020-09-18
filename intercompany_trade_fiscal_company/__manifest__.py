576cd4cea859d277c267c83f18b914b6fb6f5087
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-09-18 16:48:08 +0200

    [WIP]

--- intercompany_trade_fiscal_company/__manifest__.py
+++ intercompany_trade_fiscal_company/__manifest__.py
@@ -9,33 +9,14 @@
     "website": "http://www.grap.coop",
     "license": "AGPL-3",
     "depends": [
-        # "base_fiscal_company",
+        "fiscal_company_base",
         "intercompany_trade_base",
-        "account",
-        ##        'purchase',
-        ##        'sale',
-        ##        'simple_tax_account',
-        # Force implicit dependency for the correct order of
-        # the call of the function product_id_change
-        ##        'simple_tax_sale',
+        "account"
     ],
     "data": [
-        #        'security/ir_rule.xml',
-        #        'security/ir.model.access.csv',
-        #        "views/view_account_account.xml",
-        # 'views/view_fiscal_company_transcoding_account.xml',
-        #        "views/view_intercompany_trade_config.xml",
-        #        "views/view_res_company.xml",
-    ],
-    "demo": [
-        #        "demo/account_tax.xml",
-        #        "demo/account_account.xml",
-        #        "demo/res_company.xml",
-        #        # 'demo/fiscal_company_transcoding_account.xml',
-        #        "demo/res_users.xml",
-        #        "demo/res_groups.xml",
-        #        "demo/intercompany_trade_config.xml",
-        #        "demo/product_product.xml",
+       "views/view_account_account.xml",
+       "views/view_intercompany_trade_config.xml",
+       "views/view_res_company.xml",
     ],
     "auto_install": True,
     "installable": True,
