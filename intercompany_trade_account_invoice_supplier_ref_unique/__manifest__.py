f2e118fd629f65931d6308f669f2cb6ffb491fe5
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-12-08 12:15:50 +0100

    [FIX] tests; [REM] useless demo_intercompany option ; [ADD] glue module intercompany_trade_account_invoice_supplier_ref_unique to avoid warning in test

---
+++ intercompany_trade_account_invoice_supplier_ref_unique/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2018 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Intercompany Trade - Account Invoice Supplier Ref Unique",
+    "version": "12.0.1.0.1",
+    "category": "Intercompany Trade",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "license": "AGPL-3",
+    "depends": ["intercompany_trade_account", "account_invoice_supplier_ref_unique"],
+    "auto_install": True,
+    "installable": True,
+}
