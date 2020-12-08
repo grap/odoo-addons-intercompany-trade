756613655208a7b4260dd02f3963a175ea595401
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-12-08 12:15:50 +0100

    [FIX] tests; [REM] useless demo_intercompany option ; [ADD] glue module intercompany_trade_account_invoice_supplier_ref_unique to avoid warning in test

--- intercompany_trade_account/models/account_invoice_line.py
+++ intercompany_trade_account/models/account_invoice_line.py
@@ -30,7 +30,9 @@ class AccountInvoiceLine(models.Model):
         self.ensure_one()
 
         # Create according account invoice line
-        customer_product = config.get_customer_product(self.product_id)
+        customer_product = config.get_customer_product(
+            self.product_id
+        ).sudo(config.customer_user_id)
 
         if not customer_product:
             raise UserError(
@@ -41,8 +43,7 @@ class AccountInvoiceLine(models.Model):
                 % (self.product_id.code, self.product_id.name)
             )
 
-        customer_template_product = customer_product.product_tmpl_id\
-            .sudo(config.customer_user_id)
+        customer_template_product = customer_product.product_tmpl_id
 
         account_id =\
             customer_template_product._get_product_accounts()["expense"].id
@@ -54,7 +55,10 @@ class AccountInvoiceLine(models.Model):
                     " the produict of your customer doesn't have a correct"
                     " accounting setting %s - %s"
                 )
-                % (customer_template_product.code, customer_template_product.name)
+                % (
+                    customer_template_product.code,
+                    customer_template_product.name
+                )
             )
 
         vals = {
--- intercompany_trade_account/tests/test_module.py
+++ intercompany_trade_account/tests/test_module.py
@@ -44,13 +44,12 @@ class Test(TransactionCase):
             "intercompany_trade_account.intercompany_invoice"
         )
 
-    def _DISABLED_test_01_confirm_invoice_out(self):
+    def test_01_confirm_invoice_out(self):
         """Confirm an Out Invoice by the supplier must create an In Invoice"""
 
         # Confirm supplier invoice and get it's name
-        self.intercompany_invoice.sudo(self.supplier_user).with_context(
-            demo_intercompany=True
-        ).action_invoice_open()
+        self.intercompany_invoice.sudo(
+            self.supplier_user).action_invoice_open()
 
         # Try to get the customer invoice
         invoices = self.AccountInvoice.search(
