073449c803b5bb8403977069e3cd2871d8c57ff3
Quentin Dupont <perso@quentindupont.fr>
2021-02-08 15:59:22 +0100

    [MIG] Ti - Purchase

---
+++ intercompany_trade_purchase/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2021 - Today: GRAP (http://www.grap.coop)
+# @author Quentin DUPONT <quentin.dupont@grap.coop>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Intercompany Trade - Purchase",
+    "version": "12.0.1.0.2",
+    "category": "Intercompany Trade",
+    "author": "GRAP",
+    "website": "http://www.grap.coop",
+    "license": "AGPL-3",
+    "depends": ["intercompany_trade_base", "purchase"],
+    "data": [
+        "views/view_purchase_order.xml",
+    ],
+    "demo": [
+    ],
+    "auto_install": True,
+    "installable": True,
+}
---
+++ intercompany_trade_purchase/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module hides useless invoice button in purchase order.
+Unnecessary because the invoice is created by the seller.
