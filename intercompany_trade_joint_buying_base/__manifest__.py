df0b63e956b634106631ad315408ab4f434fde4b
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-06-24 12:14:43 +0200

    [ADD] new glue module intercompany_trade-joint_buying_base

---
+++ intercompany_trade_joint_buying_base/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2022 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Intercompany Trade - Joint Buying Base",
+    "version": "12.0.1.0.1",
+    "category": "Intercompany Trade",
+    "author": "GRAP",
+    "website": "https://github.com/grap/odoo-addons-intercompany-trade",
+    "license": "AGPL-3",
+    "depends": ["intercompany_trade_base", "joint_buying_base"],
+    "auto_install": True,
+    "installable": True,
+}
