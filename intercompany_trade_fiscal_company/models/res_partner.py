77eb4c4cb5880ebd9d4e532ddf59405cfad6054e
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-11-30 00:43:09 +0100

    [FIX]  intercompany_trade_fiscal_company : prevent to block creation of intercompany.trade.config settings

--- intercompany_trade_fiscal_company/models/res_partner.py
+++ intercompany_trade_fiscal_company/models/res_partner.py
@@ -16,33 +16,35 @@ class ResPartner(models.Model):
         "property_account_position_id",
     )
     def _check_intercompany_trade_same_fiscal_company_fiscal_position(self):
-        if not self.intercompany_trade:
-            if self.property_account_position_id.is_intercompany_trade_fiscal_company:
-                raise UserError(
-                    _(
-                        "It's not possible to set the fiscal position %s to this partner."
-                        " You should not use a fiscal position for intercompany trade"
-                        " between same fiscal companies."
-                    )
-                    % (self.property_account_position_id.name)
-                )
-            return
-
-        IntercompanyTradeConfig = self.env["intercompany.trade.config"]
-        same_fiscal_mother_company = any(
-            IntercompanyTradeConfig.search(
-                [
-                    "|",
-                    ("customer_partner_id", "=", self.id),
-                    ("supplier_partner_id", "=", self.id),
-                ]
-            ).mapped("same_fiscal_mother_company")
-        )
         if (
-            same_fiscal_mother_company
-            != self.property_account_position_id.is_intercompany_trade_fiscal_company
+            not self.intercompany_trade
+            and self.property_account_position_id.is_intercompany_trade_fiscal_company
         ):
             raise UserError(
-                _("It's not possible to set the fiscal position %s to this partner.")
+                _(
+                    "It's not possible to set the fiscal position '%s' to this partner.\n\n"
+                    " You should not use a fiscal position for intercompany trade"
+                    " between same fiscal companies."
+                )
+                % (self.property_account_position_id.name)
+            )
+
+        # ref "and self.property_account_position_id"
+        # Ugly Hack do not check when creating partners from
+        # the creation of intercompany.trade.config
+        # this problem should come from the other problem with
+        # property_account_position_id / no_property_account_position_id
+        # TODO: Check in V16, if we can remove all the sudo() things.
+        if (
+            self.intercompany_trade
+            and self.property_account_position_id
+            and not self.property_account_position_id.is_intercompany_trade_fiscal_company
+        ):
+            raise UserError(
+                _(
+                    "It's not possible to set the fiscal position '%s' to this partner.\n\n"
+                    " You should use a fiscal position for intercompany trade"
+                    " between same fiscal companies."
+                )
                 % (self.property_account_position_id.name)
             )
