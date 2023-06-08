ae66a3e17bbfff5ba53621e1cefcd9a94c0409d6
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-06-08 17:38:24 +0200

    [ADD] new module intercompany_trade_fiscal_company_sale to prepare correctly invoice vals, regarding account journal

---
+++ intercompany_trade_fiscal_company_sale/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright (C) 2023 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Intercompany Trade - Fiscal Company - Sale",
+    "version": "12.0.1.0.0",
+    "category": "Intercompany Trade",
+    "author": "GRAP",
+    "website": "https://github.com/grap/odoo-addons-intercompany-trade",
+    "license": "AGPL-3",
+    "depends": ["fiscal_company_base", "intercompany_trade_base", "sale"],
+    "auto_install": True,
+    "installable": True,
+}
