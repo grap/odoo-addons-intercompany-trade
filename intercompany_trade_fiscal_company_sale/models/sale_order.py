3ec971d8d596edd8bd24c1329c84d3e4cf3691e9
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-06-08 17:38:24 +0200

    [ADD] new module intercompany_trade_fiscal_company_sale to prepare correctly invoice vals, regarding account journal

---
+++ intercompany_trade_fiscal_company_sale/models/sale_order.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2023 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.multi
+    def _prepare_invoice(self):
+        vals = super()._prepare_invoice()
+        config = self.env[
+            "intercompany.trade.config"
+        ]._get_intercompany_trade_by_partner_company(
+            vals["partner_id"], vals["company_id"], "out"
+        )
+        if config:
+            vals["journal_id"] = config.sale_journal_id.id
+        return vals
