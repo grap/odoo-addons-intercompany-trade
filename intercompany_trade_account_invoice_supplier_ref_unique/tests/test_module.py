b52ebf46a623bc6046586eadcf9c09591fdba454
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-02-19 23:41:31 +0100

    [FIX] intercompany_trade: finish to rename the module in some places of the repo

--- intercompany_trade_account_invoice_supplier_ref_unique/tests/test_module.py
+++ intercompany_trade_account_invoice_supplier_ref_unique/tests/test_module.py
@@ -6,7 +6,7 @@ import logging
 
 from odoo.tests.common import TransactionCase
 
-from odoo.addons.intercompany_trade_base.tests.test_module import (
+from odoo.addons.intercompany_trade.tests.test_module import (
     TestModule as TestIntercompanyTradeBase,
 )
 
@@ -30,9 +30,9 @@ class Test(TransactionCase):
         self.AccountInvoice = self.env["account.invoice"]
 
         # Get object from xml_ids
-        self.supplier_user = self.env.ref("intercompany_trade_base.supplier_user")
+        self.supplier_user = self.env.ref("intercompany_trade.supplier_user")
 
-        self.customer_company = self.env.ref("intercompany_trade_base.customer_company")
+        self.customer_company = self.env.ref("intercompany_trade.customer_company")
 
         self.intercompany_invoice = self.env.ref(
             "intercompany_trade_account.intercompany_invoice"
