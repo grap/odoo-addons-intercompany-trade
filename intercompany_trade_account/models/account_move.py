1bd0108c2a5995a5eba63a63e962a936bec80b15
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-02-20 22:32:32 +0100

    fixup! fixup! fixup! [mig] intercompany_trade_account: remove useless feature

--- intercompany_trade_account/models/account_move.py
+++ intercompany_trade_account/models/account_move.py
@@ -5,8 +5,8 @@
 from odoo import fields, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
     intercompany_trade = fields.Boolean(
         string="Intercompany Trade",
