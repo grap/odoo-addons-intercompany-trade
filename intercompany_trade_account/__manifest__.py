e5a4d7b6e1156943c6265af487350b0a47dbe878
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-05-15 19:35:41 +0200

    fixup! fixup! --fixup=HEAD

--- intercompany_trade_account/__manifest__.py
+++ intercompany_trade_account/__manifest__.py
@@ -22,11 +22,11 @@
         "demo/product_product.xml",
         "demo/account_fiscal_position.xml",
         "demo/res_company.xml",
-        # "demo/account_invoice.xml",
     ],
     "data": [
         "views/view_account_account.xml",
         "views/view_account_fiscal_position.xml",
+        "views/view_account_fiscal_position_template.xml",
         "views/view_account_journal.xml",
         "views/view_account_move.xml",
         "views/view_res_config_settings.xml",
--- intercompany_trade_account/models/__init__.py
+++ intercompany_trade_account/models/__init__.py
@@ -2,6 +2,7 @@ from . import account_account
 from . import account_chart_template
 from . import account_account_template
 from . import account_fiscal_position
+from . import account_fiscal_position_template
 from . import account_journal
 from . import account_move
 from . import account_move_line
