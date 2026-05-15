601c9d2984c3cd6e7941a02f7edfd6db45b57dd1
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-05-15 20:17:47 +0200

    fixup! fixup! fixup! --fixup=HEAD

---
+++ intercompany_trade_account_usability/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2015 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Intercompany Trade - Account Usability",
+    "version": "16.0.1.0.0",
+    "category": "Intercompany Trade",
+    "author": "GRAP",
+    "website": "https://github.com/grap/odoo-addons-intercompany-trade",
+    "license": "AGPL-3",
+    "depends": [
+        # OCA
+        "account_usability",
+        # Custom
+        "intercompany_trade",
+    ],
+    "data": [
+        "views/view_account_account_template.xml",
+    ],
+    "auto_install": True,
+    "installable": True,
+}
