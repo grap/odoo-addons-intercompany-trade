cd084a4bd66eb08c6471ba0dcee0c6e06230e1fe
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-05-16 23:07:38 +0200

    wip

--- intercompany_trade_account/tests/__init__.py
+++ intercompany_trade_account/tests/__init__.py
@@ -1,5 +1,5 @@
-# from . import test_abstract
-# from . import test_account_move
-# from . import test_check_res_partner
-# from . import test_check_account_journal
+from . import test_abstract
+from . import test_account_move
+from . import test_check_res_partner
+from . import test_check_account_journal
 from . import test_account_chart_template
---
+++ intercompany_trade_account/tests/test_account_chart_template.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2015 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import logging
+
+from odoo.tests import tagged
+
+from .test_abstract import TestIntercompanyTradeAccountAbstract
+
+_logger = logging.getLogger(__name__)
+
+
+@tagged("post_install", "-at_install")
+class TestIntercompanyTradeAccountChartTemplate(TestIntercompanyTradeAccountAbstract):
+    def test_01_classic_partner(self):
+        chart_template = self.env["account.chart.template"].search([], limit=1)
+        if not chart_template:
+            return
+        company = self.env["res.company"].create(
+            {
+                "name": "CAE company - Test chart template",
+                "fiscal_type": "fiscal_mother",
+            }
+        )
+
+        template_account = chart_template.account_ids[0]
+        template_account.is_intercompany_trade = True
+
+        # Load chart template
+        chart_template.try_loading(company=company, install_demo=False)
+
+        # check if account setting is correct
+        new_account = self.env["account.account"].search(
+            [("company_id", "=", company.id), ("is_intercompany_trade", "=", True)]
+        )
+        self.assertEqual(len(new_account), 1)
+
+        # Note: We can not test fiscal position correct setting because demo CoA
+        # doesn't contain any fiscal position. Maybe the coA-pocalypse
+        # in most recent version will fix that.
